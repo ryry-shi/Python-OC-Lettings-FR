41fb30836f7f614049c652e39b87d307d53c94bb
ryry-shi <ryadboulahrouf84@outlook.fr>
2024-03-12 16:14:26 +0100

    feature-sentry

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -11,9 +11,14 @@ from . import views
 handler404 = views.page_not_found_view
 handler500 = views.server_error_view
 
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('profiles/', include('profiles.urls')),
     path('lettings/', include('lettings.urls')),
     path('admin/', admin.site.urls),
+    path('sentry-debug/', trigger_error),
 ]
